cda1ec5f29cc4e3752a2ae691c2263b7a74c1d57
jtakagicorp <jtakagi_corp@yahoo.co.jp>
2014-02-16 06:15:35 +0900

    tuning done

--- js/mainaudio.js
+++ js/mainaudio.js
@@ -12,12 +12,19 @@ function announce() {
 function departure() {
   audio = new Audio("audio/departure.mp3");
   audio.play();
-  setTimeout("shushu()", 9000);
+  setTimeout("shushu_weak()", 5000);
+}
+
+function shushu_weak() {
+  audio = new Audio("audio/shushu2_weak.mp3");
+  audio.loop = true;
+  audio.play();
+  setTimeout("shushu()", 5000);
 }
 
 function shushu() {
+  if (audio != null) audio.pause();
   audio = new Audio("audio/shushu2.mp3");
   audio.loop = true;
   audio.play();
-  console.log(audio);
 }
